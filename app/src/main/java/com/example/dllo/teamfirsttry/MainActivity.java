7de5557ba40c81e710baa4c03a3c9314dc48b146
徐林斌 <837495010@qq.com>
2016-08-11 18:01:01 +0800

    驻俄界面

--- app/src/main/java/com/example/dllo/teamfirsttry/MainActivity.java
+++ app/src/main/java/com/example/dllo/teamfirsttry/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //主界面搭建
+//搭建
     }
 }
